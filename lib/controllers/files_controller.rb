2a7432f470295baf3b02eafb47513475ac315d0a
guillermo varela <guillovarela16@gmail.com>
2020-03-13 11:36:28 -0300

    finish

--- lib/controllers/files_controller.rb
+++ lib/controllers/files_controller.rb
@@ -1,6 +1,5 @@
 require_relative "../views/files_view"
 require_relative "../models/file"
-require_relative "../repositories/folder_repository"
 
 class FilesController
   def initialize(files_repo)
@@ -13,16 +12,6 @@ class FilesController
     content = @view.ask_for_content
     file = File.new(title: title, content: content)
     @files_repo.add(file)
-  end
-
-  # metodo recientemente creado
-  def create_folder_file
-    title = @view.ask_for_title
-    content = @view.ask_for_content
-    file = File.new(title: title, content: content)
-    persisted_folder = @folders_repo.find_by_name(folder.name)
-    persisted_folder.add_file(file)
-    @file_repo.add(file)
   end
 
   def show

d39eeda88ce603bc92426e2e889a30865a9fe4e1
guillermo varela <guillovarela16@gmail.com>
2020-03-13 12:16:37 -0300

    change the show

--- lib/controllers/files_controller.rb
+++ lib/controllers/files_controller.rb
@@ -12,7 +12,7 @@ class FilesController
 
   def show(name)
     files = @files_repo.find_by_title(name)
-    p files.content unless files.nil?
+    puts files.content unless files.nil?
   end
 
   def destroy(name)
